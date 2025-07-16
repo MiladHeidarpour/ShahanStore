791649b32bfe6bf4900f219d114915f32c64f0c6
Milad Heidarpour <mld.heidarpour@gmail.com>
2025-07-16 16:00:48 +0330

    feat(domain): Add Category aggregate and its repository interface
    This commit introduces the core Category aggregate root and its related components within the domain layer.
    
    - Created the `Category` aggregate root with its properties and methods.
    - Created the `CategoryAttribute` child entity to support dynamic attributes.
    - Added business logic to the `Category` class to manage attributes (Add, Edit, Remove).
    - Defined the specific `ICategoryRepository` interface to abstract data access.

---
+++ Src/ShahanStore.Domain/Categories/Category.cs
@@ -0,0 +1,101 @@
+﻿using Common.Domain.Bases;
+using Common.Domain.Exceptions;
+using Common.Domain.Utilities;
+using Common.Domain.ValueObjects;
+
+namespace ShahanStore.Domain.Categories;
+
+public class Category : AggregateRoot
+{
+    public string Title { get; private set; }
+    public string Slug { get; private set; }
+    public Guid? ParentId { get; private set; }
+    public string? BannerImg { get; private set; }
+    public string? Icon { get; private set; }
+    public SeoData SeoData { get; private set; }
+
+    private readonly List<CategoryAttribute> _categoryAttributes = new();
+    public IReadOnlyCollection<CategoryAttribute> CategoryAttributes => _categoryAttributes.AsReadOnly();
+
+
+
+    private Category() { }
+    public Category(string title, string slug, Guid? parentId, string? bannerImg, string? icon, SeoData seoData)
+    {
+        slug = slug.ToSlug();
+        Guard(title, slug);
+        Title = title;
+        Slug = slug;
+        ParentId = parentId;
+        BannerImg = bannerImg;
+        Icon = icon;
+        SeoData = seoData;
+    }
+   
+    public void Edit(string title, string slug, Guid? parentId, SeoData seoData)
+    {
+        slug = slug.ToSlug();
+        Guard(title, slug);
+        Title = title;
+        Slug = slug;
+        ParentId = parentId;
+        SeoData = seoData;
+    }
+
+    public void ChangeBanner(string newBanner)
+    {
+        BannerImg = newBanner;
+    }
+
+    public void ChangeIcon(string newIcon)
+    {
+        Icon = newIcon;
+    }
+
+
+
+    //CategoryAttribute
+    public void AddAttribute(string attributeName, List<string> possibleValues)
+    {
+        if (_categoryAttributes.Any(a => a.Name == attributeName))
+        {
+            throw new InvalidDomainDataException($"Attribute '{attributeName}' already exists in this category.",nameof(CategoryAttributes));
+        }
+
+        var newAttribute = new CategoryAttribute(attributeName, possibleValues, Id);
+        _categoryAttributes.Add(newAttribute);
+    }
+
+    public void EditAttribute(Guid attributeId, string newName, List<string> newValues)
+    {
+        var attribute = _categoryAttributes.FirstOrDefault(a => a.Id == attributeId);
+        if (attribute == null)
+        {
+            throw new InvalidDomainDataException("Attribute not found in this category.", nameof(CategoryAttributes));
+        }
+
+        if (_categoryAttributes.Any(a => a.Id != attributeId && a.Name == newName))
+        {
+            throw new InvalidDomainDataException($"Attribute '{newName}' already exists in this category.", nameof(CategoryAttributes));
+        }
+
+        attribute.Edit(newName, newValues);
+    }
+
+    public void RemoveAttribute(Guid attributeId)
+    {
+        var attribute = _categoryAttributes.FirstOrDefault(a => a.Id == attributeId);
+        if (attribute != null)
+        {
+            _categoryAttributes.Remove(attribute);
+        }
+    }
+
+
+    //Guard
+    private void Guard(string title, string slug)
+    {
+        DomainGuard.AgainstNullOrEmpty(title, nameof(Title));
+        DomainGuard.AgainstNullOrEmpty(slug, nameof(Slug));
+    }
+}
---
+++ Src/ShahanStore.Domain/Categories/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Common.Domain.Repositories;
+
+namespace ShahanStore.Domain.Categories;
+
+public interface ICategoryRepository:IGenericRepository<Category>
+{
+    
+}
