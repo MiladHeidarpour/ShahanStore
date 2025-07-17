0b8bce2445bf993d5df9f01d37b78f3d3877da10
Milad Heidarpour <mld.heidarpour@gmail.com>
2025-07-17 17:36:14 +0330

    feat(common): Add common application layer
    This commit introduces the Common.Application project, which provides shared abstractions and behaviors for the application layer.
    
    - Defines ICommand and ICommandHandler interfaces to standardize the CQRS pattern.
    - Implements a robust, immutable OperationResult pattern for handling use case outcomes.
    - Adds a MediatR ValidationBehavior for automatic and centralized command validation.
    - Includes a custom ValidationException for structured error handling.

---
+++ Commons/Common.Application/Abstractions/Messaging/Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using Common.Application.Models.Results;
+using MediatR;
+
+namespace Common.Application.Abstractions.Messaging.Commands;
+
+public interface ICommand : IRequest<OperationResult>
+{
+}
+
+public interface ICommand<TData> : IRequest<OperationResult<TData>>
+{
+}
---
+++ Commons/Common.Application/Abstractions/Messaging/Commands/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Common.Application.Models.Results;
+using MediatR;
+
+namespace Common.Application.Abstractions.Messaging.Commands;
+
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, OperationResult> where TCommand : ICommand
+{
+}
+
+public interface ICommandHandler<TCommand, TResponseData> : IRequestHandler<TCommand, OperationResult<TResponseData>> where TCommand : ICommand<TResponseData>
+{
+}
